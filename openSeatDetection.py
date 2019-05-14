6dd564732c17333979136a441c4b4f08362e117a
Adam <44130552+adsalad@users.noreply.github.com>
2019-05-14 04:07:33 -0400

    Update openSeatDetection.py
    the previous commit only worked if the course requested to be checked was the first returned result in the table (of the refreshed website page), this commit allows for any course to be searched and checked for an open spot regardless of how many tables/rows there are

--- openSeatDetection.py
+++ openSeatDetection.py
@@ -8,39 +8,45 @@ account_sid = ""
 auth_token = ""
 client = Client(account_sid, auth_token)
 
-# Create a new instance of the Firefox driver
-driver = webdriver.Chrome('/Users/User/Downloads/chromedriver')
+def check_seats(course_number, class_number):
 
-# go to the google home page
-driver.get("https://studentservices.uwo.ca/secure/timetables/SummerTT/ttindex.cfm")
+    # Create a new instance of the Firefox driver
+    driver = webdriver.Chrome('/Users/User/Downloads/chromedriver')
 
-# find the element that's name attribute is q (the google search box)
-inputElement = driver.find_element_by_id("inputCatalognbr")
+    # go to the google home page
+    driver.get("https://studentservices.uwo.ca/secure/timetables/SummerTT/ttindex.cfm")
 
-# type in the search
-inputElement.send_keys("")
+    # find the element that's name attribute is q (the google search box)
+    inputElement = driver.find_element_by_id("inputCatalognbr")
 
-# submit the form (although google automatically searches now without submitting)
-inputElement.submit()
+    # type in the search
+    inputElement.send_keys(course_number)
 
-try:
-    siteSource = driver.page_source
-    refinedSource = BeautifulSoup(siteSource, "html.parser")
-    table = refinedSource.find("table", {"class": "table table-striped"})
+    # submit the form (although google automatically searches now without submitting)
+    inputElement.submit()
 
-    dataList = []
-    for sibling in table.tbody.tr.td.next_siblings:
-        dataList.append(sibling)
-    dataList = [str(elem) for elem in dataList]
-    dataList = [x.replace(' ', '') for x in dataList]
+    try:
+        siteSource = driver.page_source
+        refinedSource = BeautifulSoup(siteSource, "html.parser")
+        allRows = refinedSource.find_all('tr')
+        userRows = [t for t in alRrows if t.find_all(text=re.compile(class_number))]
+        dataList = [str(elem) for elem in userRows]
 
-    if any("NotFull" in x for x in dataList):
-        message = client.messages.create(to="+", from_="+", body="Course is not Full!")
+        if any("Not Full" in x for x in dataList):
+            message = client.messages.create(to="+", from_="+", body="Course is not Full!")
 
-except selenium.common.exceptions.NoSuchElementException:
-    print("Keyword Does Not Exist")
-finally:
-    driver.quit()
+    except selenium.common.exceptions.NoSuchElementException:
+        print("Keyword Does Not Exist")
+    finally:
+        driver.quit()
 
-# why does it check first row only????
-# research lists
+
+def main():
+    print("Hey! This is a tool that will automatically check and notify you when a course seat opens up /n")
+    print("First provide us with the 'Course Number', such as '1027' for 'CS1027', and provide us with the 'Class Number' which can be"
+          "found manually by searching the timetable")
+    courseNumber = input("Type in the Course Number here: ")
+    classNumber = input("And type in the Class Number here: ")
+    check_seats(courseNumber, classNumber)
+
+ main()
